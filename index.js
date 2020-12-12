a4a75ff2d83d32ead665f3ae07332057fdf213e7
Hasan Sarfaraz <mail.hasank@gmail.com>
2020-12-13 00:43:06 +1100

    Second commit

--- index.js
+++ index.js
@@ -55,3 +55,32 @@ newTaskForm.addEventListener('submit', (event) => {
 function validFormFieldInput(data) {
     return data !== null && data !== '';
 }
+
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+         theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+});
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
