33a2ae24fbca0051c2942be9f26030da3691e626
Hasan Sarfaraz <mail.hasank@gmail.com>
2020-12-16 10:07:48 +1100

    Minor changes 1

--- js/index.js
+++ js/index.js
@@ -73,14 +73,15 @@ $(document).ready(function () {
     });
 
 });
-$(document).ready(function () {
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-    });
+// $(document).ready(function () {
+//     $("#sidebar").mCustomScrollbar({
+//         theme: "minimal"
+//     });
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar, #content').toggleClass('active');
-        $('.collapse.in').toggleClass('in');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
-});
+//     $('#sidebarCollapse').on('click', function () {
+//         $('#sidebar, #content').toggleClass('active');
+//         $('.collapse.in').toggleClass('in');
+//         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+//     });
+// });
+
